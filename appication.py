f0f964b5556eba514c62e2a181f1b091d4ef6a73
karanjeetsingh2548 <68258787+karanjeetsingh2548@users.noreply.github.com>
2020-07-18 08:22:27 +0530

    Update appication.py

--- appication.py
+++ appication.py
@@ -1,12 +1,9 @@
- import os
-  import requests
 
   from flask import Flask, jsonify, render_template, request
   from flask_socketio import SocketIO, emit
 
   app = Flask(__name__)
-  app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-  socketio = SocketIO(app)
+    socketio = SocketIO(app)
 
   votes = {"yes": 0, "no": 0, "maybe": 0}
 
